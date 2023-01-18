9c376d0c8feafc25242fbc41e1d2ac8be6baa182
FabioSantos99 <120610757+FabioSantos99@users.noreply.github.com>
2023-01-17 23:30:05 -0300

    ok

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,10 +1,11 @@
- var c = 1
+/* var c = 1
  do {
     console.log(`Passo ${c}`) 
     c++ // c = c + 1
  } while (c <=6)
 
-/*
+ou
+
 var c = 1
 while (c <=5) {
     console.log(`Passo ${c}`)
@@ -20,3 +21,13 @@ console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?') */
+
+/*for (var c = 1;c <= 150;c++) {
+    console.log(c)
+} */
+
+console.log('vai começar...')
+for(var c = 1; c<=4;c++) {
+    console.log(c)
+}
+console.log('fim') 

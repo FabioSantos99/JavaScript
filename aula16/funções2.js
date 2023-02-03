87bea9fb2a181cb789e6277dd250496179aebe1b
FabioSantos99 <120610757+FabioSantos99@users.noreply.github.com>
2023-02-03 20:32:10 -0300

    ok

---
+++ aula16/funções2.js
@@ -0,0 +1,14 @@
+function soma(n1=0,n2=0) {
+    return n1 + n2
+}
+
+console.log(soma(5+5))
+
+function parimpar(n) {
+    if (n%2 == 0){
+        return 'Par!'
+    } else {
+    return 'Impar!'
+}
+}
+console.log(parimpar())
